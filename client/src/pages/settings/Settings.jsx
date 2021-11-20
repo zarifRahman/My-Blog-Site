189a7d0559bf1690cd1d967fa15202d7eb86c142
Zarif Shahriar <zarif.rahman@circlefintech.com>
2021-11-20 14:09:40 +0600

    Add: user

--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -1,7 +1,41 @@
 import "./settings.css";
 import Sidebar from "../../components/sidebar/Sidebar";
+import { Context } from "../../context/Context";
+import { useContext, useState } from "react";
+import axios from "axios";
 
 export default function Settings() {
+  const { user } = useContext(Context);
+  const [file, setFile] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const updatedUser = {
+      username: user._id,
+      email,
+      password
+    };
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      updatedUser.profilePic = filename;
+      try {
+        await axios.post("/upload", data)
+      } catch (err) {
+
+      }
+    }
+    try {
+      axios.put("/users" + user._id, updatedUser);
+    } catch (err) {
+
+    }
+  }
   return (
     <div className="settings">
       <div className="settingsWrapper">
@@ -9,29 +43,35 @@ export default function Settings() {
           <span className="settingsTitleUpdate">Update Your Account</span>
           <span className="settingsTitleDelete">Delete Account</span>
         </div>
-        <form className="settingsForm">
+        <form className="settingsForm" onSubmit={handleSubmit}>
           <label>Profile Picture</label>
           <div className="settingsPP">
             <img
-              src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              src={user.profilePic}
               alt=""
             />
             <label htmlFor="fileInput">
               <i className="settingsPPIcon far fa-user-circle"></i>{" "}
             </label>
-            <input
+            {/* <input
               id="fileInput"
               type="file"
               style={{ display: "none" }}
               className="settingsPPInput"
+            /> */}
+            <input
+              id="fileInput"
+              type="file"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
             />
           </div>
           <label>Username</label>
-          <input type="text" placeholder="Safak" name="name" />
+          <input type="text" placeholder={user.username} name="name" onChange={e => setUsername(e.target.value)} />
           <label>Email</label>
-          <input type="email" placeholder="safak@gmail.com" name="email" />
+          <input type="email" placeholder={user.email} name="email" onChange={e => setEmail(e.target.value)} />
           <label>Password</label>
-          <input type="password" placeholder="Password" name="password" />
+          <input type="password" placeholder="Password" name="password" onChange={e => setPassword(e.target.value)} />
           <button className="settingsSubmitButton" type="submit">
             Update
           </button>
